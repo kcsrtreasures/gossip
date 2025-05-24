af398f6e5fc3081a94e8c74c18a75cbfe5c6f8d2
kcsrtreasures <kcsrdev@gmail.com>
2025-05-24 09:54:42 +0800

    splat

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,9 @@ import express from "express"
 import path from "path"
 
 import { connectDB } from "./lib/db.js"
+import { app, server } from "./lib/socket.js"
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
-import { app, server } from "./lib/socket.js"
 
 dotenv.config()
 
@@ -32,7 +32,7 @@ app.use("/api/messages", messageRoutes)
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-    app.get("*", (req, res) => {
+    app.get("/*splat", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
     })
 }
