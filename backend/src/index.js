b87b41d3d18dcd84598f3e24ae32d5001062a440
kcsrtreasures <kcsrdev@gmail.com>
2025-08-14 15:57:16 +0800

    origin changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,7 +30,7 @@ app.use(cookieParser())
 app.use(cors({
   origin: function (origin, callback) {
     // Allow requests with no origin (e.g., mobile apps, curl)
-    if (allowedOrigins.includes(origin)) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("CORS error: Not allowed - " + origin));
