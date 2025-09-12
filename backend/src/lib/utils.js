a3270f00bd097e8af9b14033143b58f5482642a8
kcsrtreasures <kcsrdev@gmail.com>
2025-09-12 11:14:17 +0800

    utils revert

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,8 +8,8 @@ export const generateToken=(userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 14 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
-        secure: process.env.NODE_ENV === "production"
+        sameSite: "None",
+        secure: false
     });
 
 
