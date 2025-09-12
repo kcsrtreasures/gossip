d248b9f7baaa7066613b93d30feeff8372dd3ae2
kcsrtreasures <kcsrdev@gmail.com>
2025-09-12 13:49:10 +0800

    secure true

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,7 +9,7 @@ export const generateToken=(userId, res) => {
         maxAge: 14 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: "None",
-        secure: false // true on deploy, false on local
+        secure: true // true on deploy, false on local
     });
 
 
